724524419be3b8f55d824ce5d2a23755ead95a99
Dzung Bui <dungba.sg@gmail.com>
2017-12-03 21:29:37 +0800

    refactor

--- src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
+++ src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
@@ -4,67 +4,61 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.NoSuchElementException;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.impl.GenericObjectPool;
 import org.apache.commons.pool.impl.GenericObjectPoolFactory;
+import org.joo.scorpius.Bootstrap;
+import org.joo.scorpius.support.bootstrap.AbstractBootstrap;
+import org.joo.scorpius.support.bootstrap.CompositionBootstrap;
+import org.joo.scorpius.support.exception.BootstrapInitializationException;
+import org.joo.scorpius.support.typesafe.TypeSafeBootstrap;
 import org.joo.scorpius.support.vertx.VertxBootstrap;
 import org.joo.scorpius.test.mysql.triggers.MysqlTestTrigger;
 import org.joo.scorpius.trigger.handle.disruptor.DisruptorHandlingStrategy;
 
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
 
 import io.vertx.core.VertxOptions;
 
-public class MysqlVertxBootstrap extends VertxBootstrap {
+public class MysqlVertxBootstrap extends CompositionBootstrap {
 
-    private Config config;
+    protected void configureBootstraps(List<Bootstrap> bootstraps) {
+        bootstraps.add(new TypeSafeBootstrap());
+        bootstraps.add(AbstractBootstrap.from(this::configureMysql));
+        bootstraps.add(AbstractBootstrap.from(this::configureTriggers));
+        bootstraps.add(new VertxBootstrap(new VertxOptions().setEventLoopPoolSize(8), 8080));
+    }
 
-    public void run() {
-        configureConfiguration();
-
+    private void configureMysql() {
         try {
-            configureMysql();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            Config applicationConfig = applicationContext.getInstance(Config.class);
+            SimpleMysqlPoolFactory dbPoolFactory = new SimpleMysqlPoolFactory(
+                    applicationConfig.getString("jdbc.connectionString"), applicationConfig.getString("jdbc.user"),
+                    applicationConfig.getString("jdbc.passwd"));
+
+            GenericObjectPool.Config config = new GenericObjectPool.Config();
+            config.maxActive = 10;
+            config.testOnBorrow = true;
+            config.testWhileIdle = true;
+            config.timeBetweenEvictionRunsMillis = 10000;
+            config.minEvictableIdleTimeMillis = 60000;
+
+            GenericObjectPoolFactory<Connection> genericObjectPoolFactory = new GenericObjectPoolFactory<>(
+                    dbPoolFactory, config);
+            ObjectPool<Connection> dbPool = genericObjectPoolFactory.createPool();
+
+            applicationContext.override(ObjectPool.class, dbPool);
+            prepareDb(dbPool);
+        } catch (Exception ex) {
+            throw new BootstrapInitializationException(ex);
         }
-
-        configureTriggers();
-
-        VertxOptions options = new VertxOptions().setEventLoopPoolSize(8);
-        configureServer(options, 8080);
     }
 
-    private void configureConfiguration() {
-        config = ConfigFactory.load();
-        applicationContext.override(Config.class, config);
-    }
-
-    private void configureMysql() throws NoSuchElementException, IllegalStateException, Exception {
-        SimpleMysqlPoolFactory dbPoolFactory = new SimpleMysqlPoolFactory(config.getString("jdbc.connectionString"),
-                config.getString("jdbc.user"), config.getString("jdbc.passwd"));
-
-        GenericObjectPool.Config config = new GenericObjectPool.Config();
-        config.maxActive = 10;
-        config.testOnBorrow = true;
-        config.testWhileIdle = true;
-        config.timeBetweenEvictionRunsMillis = 10000;
-        config.minEvictableIdleTimeMillis = 60000;
-
-        GenericObjectPoolFactory<Connection> genericObjectPoolFactory = new GenericObjectPoolFactory<>(dbPoolFactory,
-                config);
-        ObjectPool<Connection> dbPool = genericObjectPoolFactory.createPool();
-
-        applicationContext.override(ObjectPool.class, dbPool);
-        prepareDb(dbPool);
-    }
-
-    private void prepareDb(ObjectPool<Connection> dbPool)
-            throws NoSuchElementException, IllegalStateException, Exception {
+    private void prepareDb(ObjectPool<Connection> dbPool) throws Exception {
         Connection connection = dbPool.borrowObject();
         try {
             String query = null;
