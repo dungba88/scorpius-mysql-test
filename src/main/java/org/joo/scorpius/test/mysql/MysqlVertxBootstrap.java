ddf68734c3a88e3eedddd138f6405dffef5e817b
Dzung Bui <dungba.sg@gmail.com>
2017-12-08 22:44:12 +0800

    use datasource instead of common pool directly

--- src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
+++ src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
@@ -6,10 +6,10 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.pool.ObjectPool;
-import org.apache.commons.pool.impl.GenericObjectPool;
-import org.apache.commons.pool.impl.GenericObjectPoolFactory;
 import org.joo.scorpius.Bootstrap;
 import org.joo.scorpius.support.bootstrap.AbstractBootstrap;
 import org.joo.scorpius.support.bootstrap.CompositionBootstrap;
@@ -17,12 +17,12 @@ import org.joo.scorpius.support.exception.BootstrapInitializationException;
 import org.joo.scorpius.support.typesafe.TypeSafeBootstrap;
 import org.joo.scorpius.support.vertx.VertxBootstrap;
 import org.joo.scorpius.test.mysql.triggers.MysqlTestTrigger;
-import org.joo.scorpius.trigger.handle.disruptor.DisruptorHandlingStrategy;
 
-import com.lmax.disruptor.YieldingWaitStrategy;
 import com.typesafe.config.Config;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.ext.web.Router;
 
 public class MysqlVertxBootstrap extends CompositionBootstrap {
 
@@ -30,51 +30,47 @@ public class MysqlVertxBootstrap extends CompositionBootstrap {
         bootstraps.add(new TypeSafeBootstrap());
         bootstraps.add(AbstractBootstrap.from(this::configureMysql));
         bootstraps.add(AbstractBootstrap.from(this::configureTriggers));
-        bootstraps.add(new VertxBootstrap(new VertxOptions().setEventLoopPoolSize(8), 8080));
+        bootstraps.add(new VertxBootstrap(new VertxOptions().setEventLoopPoolSize(8).setWorkerPoolSize(8), 1912) {
+            
+            @Override
+            protected Router configureRoutes(final Vertx vertx) {
+                Router restAPI = super.configureRoutes(vertx);
+                restAPI.get("/").handler(rc -> rc.response().end());
+                return restAPI;
+            }
+        });
     }
 
     private void configureMysql() {
         try {
             Config applicationConfig = applicationContext.getInstance(Config.class);
-            SimpleMysqlPoolFactory dbPoolFactory = new SimpleMysqlPoolFactory(
-                    applicationConfig.getString("jdbc.connectionString"), applicationConfig.getString("jdbc.user"),
-                    applicationConfig.getString("jdbc.passwd"));
 
-            GenericObjectPool.Config config = new GenericObjectPool.Config();
-            config.maxActive = 10;
-            config.testOnBorrow = true;
-            config.testWhileIdle = true;
-            config.timeBetweenEvictionRunsMillis = 10000;
-            config.minEvictableIdleTimeMillis = 60000;
+            BasicDataSource dataSource = new BasicDataSource();
+            dataSource.setUrl(applicationConfig.getString("jdbc.connectionString"));
+            dataSource.setUsername(applicationConfig.getString("jdbc.user"));
+            dataSource.setPassword(applicationConfig.getString("jdbc.passwd"));
+            dataSource.setMaxTotal(200);
 
-            GenericObjectPoolFactory<Connection> genericObjectPoolFactory = new GenericObjectPoolFactory<>(
-                    dbPoolFactory, config);
-            ObjectPool<Connection> dbPool = genericObjectPoolFactory.createPool();
-
-            applicationContext.override(ObjectPool.class, dbPool);
-            prepareDb(dbPool);
+            applicationContext.override(DataSource.class, dataSource);
+            prepareDb(dataSource);
         } catch (Exception ex) {
             throw new BootstrapInitializationException(ex);
         }
     }
 
-    private void prepareDb(ObjectPool<Connection> dbPool) throws Exception {
-        Connection connection = dbPool.borrowObject();
-        try {
-            String query = null;
-            try (InputStream is = new FileInputStream("src/main/resources/table.sql")) {
-                query = IOUtils.toString(is, "utf-8");
-            }
-            try (Statement stmt = connection.createStatement()) {
-                stmt.executeUpdate(query);
-            }
-        } finally {
-            dbPool.returnObject(connection);
+    private void prepareDb(BasicDataSource dataSource) throws Exception {
+        Connection connection = dataSource.getConnection();
+        String query = null;
+        try (InputStream is = new FileInputStream("src/main/resources/table.sql")) {
+            query = IOUtils.toString(is, "utf-8");
+        }
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(query);
         }
     }
 
     private void configureTriggers() {
-        triggerManager.setHandlingStrategy(new DisruptorHandlingStrategy(8192, new YieldingWaitStrategy()));
+//        triggerManager.setHandlingStrategy(new DisruptorHandlingStrategy(65536, new BusySpinWaitStrategy()));
         triggerManager.registerTrigger("greet_java").withAction(MysqlTestTrigger::new);
     }
 }
--- src/main/java/org/joo/scorpius/test/mysql/triggers/MysqlTestTrigger.java
+++ src/main/java/org/joo/scorpius/test/mysql/triggers/MysqlTestTrigger.java
@@ -10,7 +10,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.commons.pool.ObjectPool;
+import javax.sql.DataSource;
+
 import org.joo.scorpius.support.exception.TriggerExecutionException;
 import org.joo.scorpius.test.mysql.dto.MysqlTestRequest;
 import org.joo.scorpius.test.mysql.dto.MysqlTestResponse;
@@ -31,34 +32,35 @@ public class MysqlTestTrigger extends AbstractTrigger<MysqlTestRequest, MysqlTes
         CompletableFuture.runAsync(() -> getUserSync(executionContext), executor);
     }
 
-    @SuppressWarnings("unchecked")
     public void getUserSync(TriggerExecutionContext executionContext) {
-        ObjectPool<Connection> dbPool = executionContext.getApplicationContext().getInstance(ObjectPool.class);
+        DataSource dataSource = executionContext.getApplicationContext().getInstance(DataSource.class);
         Connection conn;
         try {
-            conn = dbPool.borrowObject();
-        } catch (Exception e) {
-            executionContext.fail(new TriggerExecutionException(e));
+            conn = dataSource.getConnection();
+        } catch (SQLException ex) {
+            executionContext.fail(new TriggerExecutionException(ex));
             return;
         }
         try {
             List<User> users = new ArrayList<>();
             try (Statement stmt = conn.createStatement()) {
-                ResultSet rs = stmt.executeQuery("select * from users");
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String name = rs.getString("name");
-                    users.add(new User(id, name));
+                try (ResultSet rs = stmt.executeQuery("select * from users where id < 10")) {
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String name = rs.getString("name");
+                        users.add(new User(id, name));
+                    }
                 }
+                // stmt.executeUpdate("insert into users (name) values('hello')");
             } catch (SQLException e) {
                 executionContext.fail(new TriggerExecutionException(e));
             }
             executionContext.finish(new MysqlTestResponse(users.toArray(new User[0])));
         } finally {
             try {
-                dbPool.returnObject(conn);
-            } catch (Exception e) {
-                executionContext.fail(new TriggerExecutionException(e));
+                conn.close();
+            } catch (SQLException e) {
+                // do nothing
             }
         }
     }
