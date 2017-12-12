cd0ff5c3b47a91f7116261345aa27d45a30b96b6
griever <griever@dungs-mbp>
2017-12-12 09:06:06 +0800

    use vertx jdbc

--- src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
+++ src/main/java/org/joo/scorpius/test/mysql/MysqlVertxBootstrap.java
@@ -2,13 +2,8 @@ package org.joo.scorpius.test.mysql;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.io.IOUtils;
 import org.joo.scorpius.Bootstrap;
 import org.joo.scorpius.support.bootstrap.AbstractBootstrap;
@@ -23,15 +18,19 @@ import com.typesafe.config.Config;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLClient;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 
 public class MysqlVertxBootstrap extends CompositionBootstrap {
 
-    protected void configureBootstraps(List<Bootstrap> bootstraps) {
+    @Override
+    protected void configureBootstraps(List<Bootstrap<?>> bootstraps) {
         bootstraps.add(new TypeSafeBootstrap());
-        bootstraps.add(AbstractBootstrap.from(this::configureMysql));
         bootstraps.add(AbstractBootstrap.from(this::configureTriggers));
-        bootstraps.add(new VertxBootstrap(new VertxOptions(), new HttpServerOptions().setUseAlpn(true), 1912) {
+        bootstraps.add(new VertxBootstrap(new VertxOptions(), new HttpServerOptions(), 1912) {
 
             @Override
             protected Router configureRoutes(final Vertx vertx) {
@@ -40,38 +39,54 @@ public class MysqlVertxBootstrap extends CompositionBootstrap {
                 return restAPI;
             }
         });
+        bootstraps.add(AbstractBootstrap.from(this::configureMysql));
     }
 
     private void configureMysql() {
         try {
             Config applicationConfig = applicationContext.getInstance(Config.class);
+            Vertx vertx = applicationContext.getInstance(Vertx.class);
+            
+            JsonObject sqlConfig = new JsonObject();
+            sqlConfig.put("url", applicationConfig.getString("jdbc.connectionString"));
+            sqlConfig.put("user", applicationConfig.getString("jdbc.user"));
+            sqlConfig.put("password", applicationConfig.getString("jdbc.passwd"));
+            sqlConfig.put("user", applicationConfig.getString("jdbc.user"));
+            sqlConfig.put("max_statements", 1);
+            sqlConfig.put("max_statements_per_connection", 1);
+            sqlConfig.put("initial_pool_size", 15);
+            sqlConfig.put("initial_pool_size", 200);
+            SQLClient client = JDBCClient.createShared(vertx, sqlConfig);
 
-            BasicDataSource dataSource = new BasicDataSource();
-            dataSource.setUrl(applicationConfig.getString("jdbc.connectionString"));
-            dataSource.setUsername(applicationConfig.getString("jdbc.user"));
-            dataSource.setPassword(applicationConfig.getString("jdbc.passwd"));
-            dataSource.setMaxTotal(200);
-
-            applicationContext.override(DataSource.class, dataSource);
-            prepareDb(dataSource);
+            applicationContext.override(SQLClient.class, client);
+            prepareDb(client);
         } catch (Exception ex) {
             throw new BootstrapInitializationException(ex);
         }
     }
 
-    private void prepareDb(BasicDataSource dataSource) throws Exception {
-        Connection connection = dataSource.getConnection();
-        String query = null;
+    private void prepareDb(SQLClient client) throws Exception {
+        String query;
         try (InputStream is = new FileInputStream("src/main/resources/table.sql")) {
             query = IOUtils.toString(is, "utf-8");
         }
-        try (Statement stmt = connection.createStatement()) {
-            stmt.executeUpdate(query);
-        }
+        client.getConnection(res -> {
+            if (res.succeeded()) {
+                SQLConnection connection = res.result();
+                connection.update(query, null);
+            }
+        });
     }
 
     private void configureTriggers() {
 //        triggerManager.setHandlingStrategy(new DisruptorHandlingStrategy(65536, new BusySpinWaitStrategy()));
         triggerManager.registerTrigger("greet_java").withAction(MysqlTestTrigger::new);
+    }
+    
+    @Override
+    public void shutdown() {
+        SQLClient client = applicationContext.getInstance(SQLClient.class);
+        client.close();
+        super.shutdown();
     }
 }
--- src/main/java/org/joo/scorpius/test/mysql/triggers/MysqlTestTrigger.java
+++ src/main/java/org/joo/scorpius/test/mysql/triggers/MysqlTestTrigger.java
@@ -1,16 +1,10 @@
 package org.joo.scorpius.test.mysql.triggers;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import javax.sql.DataSource;
 
 import org.joo.scorpius.support.exception.TriggerExecutionException;
 import org.joo.scorpius.test.mysql.dto.MysqlTestRequest;
@@ -18,6 +12,12 @@ import org.joo.scorpius.test.mysql.dto.MysqlTestResponse;
 import org.joo.scorpius.test.mysql.dto.User;
 import org.joo.scorpius.trigger.TriggerExecutionContext;
 import org.joo.scorpius.trigger.impl.AbstractTrigger;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class MysqlTestTrigger extends AbstractTrigger<MysqlTestRequest, MysqlTestResponse> {
 
@@ -33,35 +33,26 @@ public class MysqlTestTrigger extends AbstractTrigger<MysqlTestRequest, MysqlTes
     }
 
     public void getUserSync(TriggerExecutionContext executionContext) {
-        DataSource dataSource = executionContext.getApplicationContext().getInstance(DataSource.class);
-        Connection conn;
-        try {
-            conn = dataSource.getConnection();
-        } catch (SQLException ex) {
-            executionContext.fail(new TriggerExecutionException(ex));
-            return;
-        }
-        try {
-            List<User> users = new ArrayList<>();
-            try (Statement stmt = conn.createStatement()) {
-                try (ResultSet rs = stmt.executeQuery("select * from users where id < 10")) {
-                    while (rs.next()) {
-                        int id = rs.getInt("id");
-                        String name = rs.getString("name");
-                        users.add(new User(id, name));
-                    }
+        SQLClient client = executionContext.getApplicationContext().getInstance(SQLClient.class);
+        client.getConnection(res -> openConnection(res, executionContext));
+    }
+
+    private void openConnection(AsyncResult<SQLConnection> res, TriggerExecutionContext executionContext) {
+        if (res.failed())
+            executionContext.fail(new TriggerExecutionException(res.cause()));
+        else {
+            SQLConnection connection = res.result();
+            connection.query("select * from users where id < 10", queryRes -> {
+                List<User> users = new ArrayList<>();
+                ResultSet rs = queryRes.result();
+                List<JsonArray> results = rs.getResults();
+                for (JsonArray arr : results) {
+                    int id = arr.getInteger(0);
+                    String name = arr.getString(1);
+                    users.add(new User(id, name));
                 }
-                // stmt.executeUpdate("insert into users (name) values('hello')");
-            } catch (SQLException e) {
-                executionContext.fail(new TriggerExecutionException(e));
-            }
-            executionContext.finish(new MysqlTestResponse(users.toArray(new User[0])));
-        } finally {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                // do nothing
-            }
+                executionContext.finish(new MysqlTestResponse(users.toArray(new User[0])));
+            });
         }
     }
 }
